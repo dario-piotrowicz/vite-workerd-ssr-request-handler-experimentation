7907cc305de51687ca838537b765bb550088d89d
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2023-12-13 10:04:34 +0900

    poc: integrate `vite-node`

--- preact-app/vite.config.js
+++ preact-app/vite.config.js
@@ -1,18 +1,25 @@
 import preact from "@preact/preset-vite";
-import { preactWorkerdSSR } from "preact-workerd-ssr-plugin";
+import { vitePluginViteNodeMiniflare } from "@hiogawa/vite-node-miniflare";
+import { Log } from "miniflare";
 
 console.log("vite.config.js");
 /** @type {import('vite').UserConfig} */
 export default {
 	// config options
+	clearScreen: false,
+	appType: "custom",
 	ssr: {
-		target: "webworker",
 		noExternal: true,
-		optimizeDeps: {
-			include: ["preact", "preact-render-to-string"],
-		},
 	},
-	plugins: [preact(), preactWorkerdSSR()],
+	plugins: [
+		preact(),
+		vitePluginViteNodeMiniflare({
+			entry: "/worker-entry.ts",
+			miniflareOptions(options) {
+				options.log = new Log();
+			}
+		}),
+	],
 	build: {
 		minify: false,
 		ssrEmitAssets: true,
---
+++ preact-app/worker-entry.ts
@@ -0,0 +1,34 @@
+import { render } from "./entry-server";
+import type { ViteNodeMiniflareClient } from "@hiogawa/vite-node-miniflare/dist/client/vite-node"
+
+export default {
+  async fetch(request: Request, env: any) {
+    const ssrHtml = render(request.url);
+    let fullHtml = wrapHtml(ssrHtml);
+    if (env.__VITE_NODE_CLIENT) {
+      const client: ViteNodeMiniflareClient = env.__VITE_NODE_CLIENT;
+      fullHtml = await client.rpc.transformIndexHtml("/", fullHtml);
+    }
+    return new Response(fullHtml, {
+      headers: {
+        "content-type": "text/html",
+      },
+    });
+  },
+};
+
+const wrapHtml = (html: string) => `
+<!doctype html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8" />
+		<link rel="icon" type="image/svg+xml" href="src/favicon.svg" />
+		<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+		<title>Vite App</title>
+	</head>
+	<body>
+		<div id="app">${html}</div>
+		<script type="module" src="./main.jsx"></script>
+	</body>
+</html>
+`;
