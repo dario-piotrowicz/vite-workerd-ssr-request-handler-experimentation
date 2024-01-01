47146c77bbf4e2389ceb9caa8640bab2e6efe4df
Dario Piotrowicz <dario@cloudflare.com>
2024-01-01 18:11:42 +0100

    make both plugins usable

--- preact-app/vite.config.js
+++ preact-app/vite.config.js
@@ -1,8 +1,37 @@
 import preact from "@preact/preset-vite";
+
+import { preactWorkerdSSR } from "preact-workerd-ssr-plugin";
+
 import { vitePluginViteNodeMiniflare } from "@hiogawa/vite-node-miniflare";
 import { Log } from "miniflare";
 
-console.log("vite.config.js");
+const selectedWorkerdPluginName = [
+  "vitePluginViteNodeMiniflare",
+  "preactWorkerdSSR",
+].find((pluginName) => process.env.pluginName === pluginName);
+
+const workerdPluginName = selectedWorkerdPluginName ?? "preactWorkerdSSR";
+
+console.log(`\nRunning Vite using the ${workerdPluginName} Vite plugin...\n`);
+
+const workerdPlugins = {
+  // This is the preactWorkerdSSR plugin defined in this monorepo
+  preactWorkerdSSR: preactWorkerdSSR(),
+  // This is an external plugin that uses vite-node under the hood
+  // see:
+  // - https://github.com/dario-piotrowicz/vite-workerd-ssr-request-handler-experimentation/pull/1
+  // - https://github.com/hi-ogawa/vite-plugins/tree/main/packages/vite-node-miniflare
+  vitePluginViteNodeMiniflare: vitePluginViteNodeMiniflare({
+    debug: true,
+    entry: "/worker-entry.ts",
+    miniflareOptions(options) {
+      options.log = new Log();
+    },
+  }),
+};
+
+const workerdPlugin = workerdPlugins[workerdPluginName];
+
 /** @type {import('vite').UserConfig} */
 export default {
 	// config options
@@ -17,13 +46,7 @@ export default {
 	},
 	plugins: [
 		preact(),
-		vitePluginViteNodeMiniflare({
-			debug: true,
-			entry: "/worker-entry.ts",
-			miniflareOptions(options) {
-				options.log = new Log();
-			}
-		}),
+		workerdPlugin,
 	],
 	build: {
 		minify: false,
